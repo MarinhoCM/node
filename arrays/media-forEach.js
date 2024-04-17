f8f9c89ca4ffaf24ee13e30b5fd4ae518d603c68
normalabs.devjunior <normalabs.td@gmail.com>
2024-04-17 18:04:33 -0300

    :tada: init

--- arrays/media-forEach.js
+++
@@ -1,9 +0,0 @@
-const notas = [10, 6.5, 8, 7.5];
-let somadasnotas = 0;
-notas.forEach(function (nota, index){
-    somadasnotas += nota;
-    //console.log(index);
-});
-const media = somadasnotas / notas.length
-console.log(`A media dessas notas é: ${media}.`)
-
--- objects/aula1/desafio01.js
+++
@@ -1,8 +0,0 @@
-const pet = {
-    nome: "Rex",
-    dataDeNascimento: "12/03/2017",
-    brinquedoFavorito: "bolinha",
-    cor: "cinza",
-};
-console.log(pet.nome);
-console.log(pet['brinquedoFavorito']);
